812bb2417f141c6418ef32c9617c5b72d8bc5160
Phillip Manwaring <phillip.manwaring@gmail.com>
2017-04-01 20:50:41 -0400

    update empty deployment bucket logic

--- stacks/delete.js
+++ stacks/delete.js
@@ -4,32 +4,27 @@ const cloudFormation = new AWS.CloudFormation({ apiVersion: '2010-05-15' });
 const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
 
 module.exports.handler = (event, context, callback) => {
-  console.log('Received event to delete stack', event);
-  getStackConfig(event)
-    .then(deleteStack)
+  const config = getStackConfig(event);
+  console.log('Received event to delete stack', config);
+
+  emptyDeploymentBucket(config.deploymentBucket)
+    .then(results => deleteStack(config.stack))
     .then( stack => callback(null, `Successfully deleted stack  ${stack}`) )
     .catch( err => callback(err) );
 };
 
 const getStackConfig = event => {
-  return Promise.resolve(JSON.parse(event.Records[0].Sns.Message));
+  return JSON.parse(event.Records[0].Sns.Message);
 };
 
-const deleteStack = config => {
-  console.log('Deleting stack with config', config);
-  return config.deploymentBucket ? deleteDeploymentBucketAndStack(config) : doDeleteStack(config.stack);
-};
-
-const deleteDeploymentBucketAndStack = config => {
-  return listObjects(config.deploymentBucket)
-          .then( objects => deleteObjects(objects, config.deploymentBucket) )
-          .then(response => doDeleteStack(config.stack) );
+const emptyDeploymentBucket = bucket => {
+  return bucket ? listObjects(bucket).then(objects => deleteObjects(objects, bucket)) : Promise.resolve('');
 };
 
 const listObjects = bucket => {
   const params = { Bucket: bucket };
   console.log('Listing objects with params', params);
-  s3.listObjectsV2(params).promise();
+  return s3.listObjectsV2(params).promise();
 };
 
 const deleteObjects = (objects, bucket) => {
@@ -43,7 +38,7 @@ const deleteObjects = (objects, bucket) => {
   return s3.deleteObjects(params).promise();
 };
 
-const doDeleteStack = stack => {
+const deleteStack = stack => {
   const params = { StackName: stack };
   console.log('Deleting stack with params', params);
   return cloudFormation.deleteStack(params).promise();
