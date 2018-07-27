47b80d7f7a36de95b3eb5f36e87b4e17a0c8d95d
Phillip Manwaring <phillip.manwaring@gmail.com>
2018-07-27 11:43:05 -0400

    clean up code

---  app/check/check.ts
+++  app/check/check.ts
@@ -1,6 +1,7 @@
 import { CloudWatchLogsEvent, Callback, Context, Handler } from 'aws-lambda';
 import * as log from 'winston';
 import { listAllStacks, publishStackForDeletion } from '../shared';
+
 log.configure({ level: process.env.LOG_LEVEL });
 
 export const handler: Handler = (event: CloudWatchLogsEvent, context: Context, callback: Callback) => {
---  app/delete/delete.ts
+++  app/delete/delete.ts
@@ -1,19 +1,18 @@
 import { SNSEvent, Callback, Context, Handler } from 'aws-lambda';
 import * as log from 'winston';
-import { DeleteRequest } from '../check/deleteRequest';
-import { emptyBuckets, deleteStack, getBucketsToEmpty } from '../shared';
+import { DeleteRequest, emptyBuckets, deleteStack, getBucketsToEmpty } from '../shared';
 
 log.configure({ level: process.env.LOG_LEVEL });
 
 export const handler: Handler = async (event: SNSEvent, context: Context, callback: Callback) => {
   try {
     const request = getStackConfig(event);
-    log.debug('Received event to delete stack', request);
+    log.debug('Received request to delete stack', request);
     log.info(`Odin has a seat in Valhalla ready for the ${request.stackName} stack`);
     const buckets = await getBucketsToEmpty(request.stackName);
     await emptyBuckets(buckets);
     await deleteStack(request.stackName);
-    return callback(null, `Successfully deleted stack ${request.stackName}`);
+    return callback(null, `Successfully deleted the ${request.stackName} stack`);
   } catch (err) {
     return callback(err);
   }
