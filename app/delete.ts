6fdf8a43958f7ffbd5cd3c248fd4fe7aea683fad
Phillip Manwaring <phillip.manwaring@gmail.com>
2018-08-12 19:47:46 -0400

    add lambda wrappers

--- app/delete.ts
+++ app/delete.ts
@@ -1,28 +1,25 @@
-import { SNSEvent, Callback, Context } from 'aws-lambda';
+import { snsWrapper, SnsSignature } from '@manwaring/lambda-wrapper';
 import { DeleteRequest, emptyBuckets, deleteStack, getBucketsToEmpty } from './shared';
 
-export async function handler(event: SNSEvent, context: Context, callback: Callback) {
+export const handler = snsWrapper(async ({ message, success, error }: SnsSignature) => {
   try {
+    // See what IOPipe supports for console methods
     console.trace('This is a trace statement');
     console.debug('This is a debug statement');
     console.info('This is an info statement');
     console.log('This is a log statement');
     console.warn('This is a warn statement');
     console.error('This is an error statement');
-    const config = getStackConfig(event);
-    await emptyBucketsInStack(config.stackName);
-    return callback(null, `Successfully deleted the ${config.stackName} stack`);
-  } catch (err) {
-    return callback(err);
-  }
-}
 
-function getStackConfig(event: SNSEvent): DeleteRequest {
-  const config: DeleteRequest = JSON.parse(event.Records[0].Sns.Message);
-  console.debug('Received request to delete stack', config);
-  console.info(`Odin has a seat in Valhalla ready for the ${config.stackName} stack`);
-  return config;
-}
+    const config: DeleteRequest = message;
+    console.debug('Received request to delete stack', config);
+    console.info(`Odin has a seat in Valhalla ready for the ${config.stackName} stack`);
+    await emptyBucketsInStack(config.stackName);
+    return success(`Successfully deleted the ${config.stackName} stack`);
+  } catch (err) {
+    return error(err);
+  }
+});
 
 async function emptyBucketsInStack(stackName: string) {
   const buckets = await getBucketsToEmpty(stackName);
