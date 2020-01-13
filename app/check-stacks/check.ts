445bc93904f72e1741a80bffa9f914ee58e547d2
Phillip Manwaring <phillip.manwaring@gmail.com>
2020-01-13 00:01:34 -0500

    dont parse cloudwatch event input

--- app/check-stacks/check.ts
+++ app/check-stacks/check.ts
@@ -3,9 +3,8 @@ import { wrapper, WrapperSignature } from '@manwaring/lambda-wrapper';
 import { CloudFormationStacks } from './stacks';
 import { PublishDeleteRequest } from './publish-delete-request';
 
-export const handler = wrapper(async ({ event, success, error }: WrapperSignature) => {
+export const handler = wrapper(async ({ event: config, success, error }: WrapperSignature) => {
   try {
-    const config = JSON.parse(event);
     const stacks = new CloudFormationStacks(config);
     const toDelete = await stacks.getStacksToDelete();
     await Promise.all(toDelete.map(stack => new PublishDeleteRequest(stack, config).publish()));
