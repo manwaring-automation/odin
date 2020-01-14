a93f7b3ae92a79648d19a02001f072eaac5ddaea
Phillip Manwaring <phillip.manwaring@gmail.com>
2020-01-13 19:02:36 -0500

    allow odin to remove stacks

--- app/delete-stack/delete.ts
+++ app/delete-stack/delete.ts
@@ -9,8 +9,7 @@ export const handler = sns(async ({ message, success, error }: SnsSignature) => 
     console.debug('Received request to delete stack', request.stackName);
     console.info(`Odin has a seat in Valhalla ready for the ${request.stackName} stack`);
     const stack = new CloudFormationStack(request);
-    // await stack.delete();
-    console.log(stack);
+    await stack.delete();
     return success(`Successfully deleted the ${request.stackName} stack`);
   } catch (err) {
     return error(err);
